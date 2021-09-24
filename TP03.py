bcf1bed2b4ac75365bbe9ecab22d28fda34aed18
Romain GREBUL <43134865+romaingrebul@users.noreply.github.com>
2021-09-24 14:50:47 +0000

    TP03

---
+++ TP03.py
@@ -0,0 +1,23 @@
+annee = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre',10,11,12] 
+
+annee.pop(9)
+annee.pop(9)
+annee.pop(9)
+annee.append("Octobre")
+annee.append("Novembre")
+annee.append("Decembre")
+print (annee)
+
+
+moisDeLannee = ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre')
+
+print (moisDeLannee[3])
+
+
+if "Mars" in moisDeLannee :
+  print("Mars")
+
+age = {"pierre" : 35 , "paul" : 32 , "Jacques" : 27 , "andre" : 23 , "david" : 22, "veronique" : 21, "sylvie" : 30, "damien" : 37} 
+
+print (age)
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #import GrebulRomainTP02
 #GrebulRomainTP02.TP02()
 
-import GrebulRomainTP02
+import TP03
