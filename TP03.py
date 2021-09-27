60c70999bbeff1812e5f52e8ab5ac39b126b1ff3
Romain GREBUL <43134865+romaingrebul@users.noreply.github.com>
2021-09-27 13:15:47 +0000

    TP03

--- TP03.py
+++ TP03.py
@@ -19,5 +19,34 @@ if "Mars" in moisDeLannee :
 
 age = {"pierre" : 35 , "paul" : 32 , "Jacques" : 27 , "andre" : 23 , "david" : 22, "veronique" : 21, "sylvie" : 30, "damien" : 37} 
 
-print (age)
+print ("l'age de sylvie est ")
+print (age["sylvie"])
+
+if "jean" in age:
+  print ("jean est ici ")
+else:
+  print ("pas la ") 
+
+
+club = {
+  "pierre durand":(1986,1.72,70),
+  "victor dupont":(1987,1.89,57),
+  "paul dupuis":(1989,1.60,92),
+  "jean rieux":(1985,1.88,77)
+  }
+
+nomSportif = input()
+dateNaissSportif = club[nomSportif][0]
+poidsSportif = club[nomSportif][2]
+tailleSportif = club[nomSportif][1]
+
+
+phrase = 'Le sportif nommé %s est né en %s, sa taille est de %sm et son poids est de %skg'
+
+print (phrase %(nomSportif,dateNaissSportif, tailleSportif, poidsSportif))
+
+
+
+
+
 
